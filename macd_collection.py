aeeffd83bfde8edc1973deaf757800437ddb571f
ktopz <kevintop@buffalo.edu>
2019-11-30 12:49:31 -0500

    Each JSON date now contains a price field

--- macd_collection.py
+++ macd_collection.py
@@ -1,12 +1,18 @@
 import requests, json
 
 def write_json(stock_name, timeframe):  
-    response = requests.get(f'https://www.alphavantage.co/query?function=MACD&symbol={stock_name}&interval={timeframe}&series_type=open&apikey=CW3985DDZ00FVGZ5')
-    if (response.status_code != 404):
-        parsed = response.json()
-        MACD_data = str(parsed['Technical Analysis: MACD'])
+    response_macd = requests.get(f'https://www.alphavantage.co/query?function=MACD&symbol={stock_name}&interval={timeframe}&series_type=close&apikey=CW3985DDZ00FVGZ5')
+    response_stockprice = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_{timeframe.upper()}&symbol={stock_name}&outputsize=full&apikey=CW3985DDZ00FVGZ5')
+    if (response_macd.status_code != 404 or response_stockprice.status_code != 404):
+        parsed_macd = response_macd.json()
+        parsed_stockprice = response_stockprice.json()
+        MACD_data = parsed_macd['Technical Analysis: MACD']
+        stock_data = parsed_stockprice[list(parsed_stockprice.keys())[1]]
+        for date in MACD_data:
+            closing_price = stock_data[date]["4. close"]
+            MACD_data[date]["price"] = closing_price
         text_file = open(f"StockData/{stock_name}_{timeframe}.json", "w")
-        text_file.write(MACD_data)
+        text_file.write(str(MACD_data))
         text_file.close()
         print(f'Successfully wrote {stock_name}_{timeframe}.json')
     else:
@@ -15,4 +21,4 @@ def write_json(stock_name, timeframe):
 
 ##### Change arguments below and run script #####
 ##### arg1 is ticker symbol, arg2 can be 'daily', 'weekly', 'monthly' #####
-write_json('LYB', 'daily')
+write_json('KMB', 'daily')
