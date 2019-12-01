742cb30db4781ceb8d2a1502dcf781b62edd81fd
ktopz <kevintop@buffalo.edu>
2019-12-01 14:23:32 -0500

    Added in SMA and RSI values

--- macd_collection.py
+++ macd_collection.py
@@ -1,21 +1,38 @@
-import requests, json
+import requests, json, time
 
 def write_json(stock_name, timeframe):  
-    response_macd = requests.get(f'https://www.alphavantage.co/query?function=MACD&symbol={stock_name}&interval={timeframe}&series_type=close&apikey=CW3985DDZ00FVGZ5')
-    response_stockprice = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_{timeframe.upper()}&symbol={stock_name}&outputsize=full&apikey=CW3985DDZ00FVGZ5')
-    if len(response_stockprice.text) < 300:
-        print("Reached API call limit, try again in a min")
-        return
+    response_macd = requests.get(f'https://www.alphavantage.co/query?function=MACD&symbol={stock_name}&interval={timeframe}&series_type=close&apikey=SKY470CWGO52HCSQ')
+    response_sma50 = requests.get(f'https://www.alphavantage.co/query?function=SMA&symbol={stock_name}&interval={timeframe}&time_period=50&series_type=close&apikey=N29DY72L98XQRYRB')
+    response_sma200 = requests.get(f'https://www.alphavantage.co/query?function=SMA&symbol={stock_name}&interval={timeframe}&time_period=200&series_type=close&apikey=CW3985DDZ00FVGZ5')
+    response_RSI = requests.get(f'https://www.alphavantage.co/query?function=RSI&symbol={stock_name}&interval={timeframe}&time_period=10&series_type=close&apikey=N29DY72L98XQRYRB')
+    response_stockprice = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_{timeframe.upper()}&symbol={stock_name}&outputsize=full&apikey=SKY470CWGO52HCSQ')
     if (response_macd.status_code != 404 or response_stockprice.status_code != 404):
         parsed_macd = response_macd.json()
+        parsed_sma50 = response_sma50.json()
+        parsed_sma200 = response_sma200.json()
+        parsed_RSI = response_RSI.json()
         parsed_stockprice = response_stockprice.json()
         MACD_data = parsed_macd['Technical Analysis: MACD']
-        stock_data = parsed_stockprice[list(parsed_stockprice.keys())[1]]
-        for date in MACD_data:
+        sma50_data = parsed_sma50['Technical Analysis: SMA']
+        sma200_data = parsed_sma200['Technical Analysis: SMA']
+        RSI_data = parsed_RSI['Technical Analysis: RSI']
+        l = []
+        for key in parsed_stockprice.keys():
+            l.append(key)
+        stock_data = parsed_stockprice[l[1]]
+        dict = {}
+        for date in sma200_data:
+            dict[date] = {}
+            dict[date]["sma50"] = sma50_data[date]["SMA"]
+            dict[date]["sma200"] = sma200_data[date]["SMA"]
+            dict[date]['MACD_Signal'] = MACD_data[date]['MACD_Signal']
+            dict[date]['MACD'] = MACD_data[date]['MACD']
+            dict[date]['MACD_Hist'] = MACD_data[date]['MACD_Hist']
+            dict[date]['RSI'] = RSI_data[date]['RSI']
             closing_price = stock_data[date]["4. close"]
-            MACD_data[date]["price"] = closing_price
+            dict[date]["price"] = closing_price
         text_file = open(f"StockData/{stock_name}_{timeframe}.json", "w")
-        text_file.write(str(MACD_data))
+        text_file.write(str(dict))
         text_file.close()
         print(f'Successfully wrote {stock_name}_{timeframe}.json')
     else:
@@ -24,5 +41,11 @@ def write_json(stock_name, timeframe):
 
 ##### Change arguments below and run script #####
 ##### arg1 is ticker symbol, arg2 can be 'daily', 'weekly', 'monthly' #####
-write_json('JPM', 'weekly')
-write_json('JPM', 'daily')
+
+list = ['LYB', 'KMB', 'JPM', 'JNJ', 'GOOGL', 'GOOG', 'FB', 'AMZN', 'AAPL']
+for name in list:
+    time.sleep(60)
+    write_json(name,'daily')
+    time.sleep(60)
+    write_json(name,'weekly')
+#write_json('MSFT', 'daily')
